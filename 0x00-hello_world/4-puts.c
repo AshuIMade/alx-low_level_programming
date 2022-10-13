c5e43081b46b02ddc65b514add5ce3ae3ef10e65
Ashenafi Yacob <asheimade@gmail.com>
2022-10-13 13:07:09 -0700

    betty doc fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /*
  * main: Entry point for program
- * description: "Programming is like building a multilingual puzzle
+ * Description: prints "\"Programming is like building a multilingual puzzle"
  * return: 0
  */
 
