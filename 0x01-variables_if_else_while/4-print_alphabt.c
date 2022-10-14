251ab21ce49e7b89222bf65b27921b34233c8976
Ashenafi Yacob <asheimade@gmail.com>
2022-10-14 13:52:18 -0700

    lowecase except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry point 
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for(c = 'a';a <= 'z';c++)
+	{
+		if( c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar("\n");
+	return(0);
+}
+
