8003ee29b3f493c12fefce8db71edb83624d8586
Ashenafi Yacob <asheimade@gmail.com>
2022-10-18 11:10:43 -0700

    fix whitespace

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- *
- * print_alphabet - function to print abc
+ * print_alphabet - function to print lowercase alphabet
  *
  * Return: 0
- *
+ *i
  */
 
 void print_alphabet(void)
 {
 	char c;
-	
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
