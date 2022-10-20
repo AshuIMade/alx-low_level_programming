f895529d281dff4aee3f1ec68e5ddf2c2b6c6c47
Ashenafi Yacob <asheimade@gmail.com>
2022-10-20 11:54:08 -0700

    print numbers 1-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: 0
+ */
+
+int print_numbers(void)
+{
+	for(int i=0; i < 10; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar("\n");
+}
