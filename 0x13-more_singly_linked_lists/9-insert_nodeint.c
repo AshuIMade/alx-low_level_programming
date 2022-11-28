7e551557f947e39439ec333baa392f6030b31742
Ashenafi Yacob <asheimade@gmail.com>
2022-11-28 10:26:52 -0800

    fix style two

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *node_index = *head;
 	listint_t *new_node, *node_after;
+
 	if (head == NULL || *head == NULL)/*Ask if there is not input*/
 		return (NULL);
 	for (nodes = 0; node_index != NULL; nodes++)/*Count the num of nodes*/
