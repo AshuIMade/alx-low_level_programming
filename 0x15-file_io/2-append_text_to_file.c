e2b02bac3e50e8afb10ba411ddf3eebdf3b66f0e
Ashenafi Yacob <asheimade@gmail.com>
2022-12-05 10:51:58 -0800

    copy content of a file to another file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,6 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
@@ -19,6 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		text_content = "";
 	for (i = 0; *(text_content + i) != '\0'; i++)
 		;
+	
 	write(file, text_content, i);
 	close(file);
 	return (1);
