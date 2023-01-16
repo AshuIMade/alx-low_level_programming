6718a499e5c988bb9f8d47296c2b5760e89e28b0
Ashenafi Yacob <asheimade@gmail.com>
2023-01-16 10:04:08 -0800

    more function prototyp

--- 0x18-dynamic_libraries/libdynamic.c
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -56,17 +56,35 @@ char *_strncat(char *dest, char *src, int n)
 	return src;
 }
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
+{
+	return dest;
+}
 
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
+{
+	return 0;
+}
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
+{
+	return s;
+}
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return dest;
+}
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
+{
+	return s;
+}
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
+{
+	return 0;
+}
 
 char *_strpbrk(char *s, char *accept);
 
